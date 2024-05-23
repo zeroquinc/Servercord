d60f106676a230cff34c759e00d48f8bcdc992c9
zeroquinc <desiler@protonmail.com>
2024-05-23 13:37:59 +0200

    feature: add user favorites API

--- test.py
+++ test.py
@@ -6,16 +6,17 @@ user = client.user('desiler')
 
 # Get the current time and the time one hour ago
 now = datetime.now()
-one_hour_ago = now - timedelta(hours=1)
+one_hour_ago = now - timedelta(hours=1200)
 
 #ratings = user.get_ratings(start_time=one_hour_ago, end_time=now)
-ratings = user.get_ratings()
-for rating in ratings:
-    print(f"Title: {rating.title}")
-    print(f"Media type: {rating.type}")
-    print(f"Season: {rating.season_id}")
-    print(f"Episode: {rating.episode_id}")
-    print(f"Rating: {rating.rated}")
-    print(f"Rated at: {rating.rated_at}")
-    print(f"Show title: {rating.show_title}")
+#ratings = user.get_ratings()
+#for rating in ratings:
+favorites = user.get_favorites(start_time=one_hour_ago, end_time=now)
+for favorite in favorites:
+    print(f"Title: {favorite.title}")
+    print(f"Media type: {favorite.type}")
+    print(f"Season: {favorite.season_id}")
+    print(f"Episode: {favorite.episode_id}")
+    print(f"Listed at: {favorite.listed_at}")
+    print(f"Show title: {favorite.show_title}")
     print()
