64245216f34632c7e8a9ebcade00725e2d7fe9e3
zeroquinc <desiler@protonmail.com>
2024-05-23 16:20:08 +0200

    chore: Initialize global variables in RadarrWebhookHandler

--- src/radarr/client.py
+++ src/radarr/client.py
@@ -5,14 +5,16 @@ class RadarrWebhookHandler:
     def __init__(self, payload, discord_bot):
         self.payload = payload
         self.discord_bot = discord_bot
+        self.initialize_global_variables()
 
-        # Global variables for the class to use in the embed creation
+    def initialize_global_variables(self):
         self.event_type = self.payload.get('eventType', 'N/A')
         self.instance_name = self.payload.get('instanceName', 'N/A')
         self.old_version = self.payload.get('previousVersion', 'N/A')
         self.new_version = self.payload.get('newVersion', 'N/A')
-        self.movie_title = self.payload.get('movie', {}).get('title', 'N/A')
-        self.movie_year = self.payload.get('movie', {}).get('year', 'N/A')
+        self.movie = self.payload.get('movie', {})
+        self.movie_title = self.movie.get('title', 'N/A')
+        self.movie_year = self.movie.get('year', 'N/A')
         self.release_data = self.payload.get('release', {})
         self.quality = self.release_data.get('quality', 'N/A')
         self.size = self.release_data.get('size', 'N/A')
@@ -20,9 +22,9 @@ class RadarrWebhookHandler:
         self.release_title = self.release_data.get('releaseTitle', 'N/A')
         self.custom_format_score = self.release_data.get('customFormatScore', 'N/A')
         self.custom_formats = self.release_data.get('customFormats', [])
-        self.tmdb_id = self.payload.get('movie', {}).get('tmdbId', 'N/A')
+        self.tmdb_id = self.movie.get('tmdbId', 'N/A')
         self.embed_title = f"{self.movie_title} ({self.movie_year})"
 
     async def handle_webhook(self):
-        logger.info(f"Processing Radarr webhook payload for event type: {self.payload.get('eventType')}")
+        logger.info(f"Processing Radarr webhook payload for event type: {self.event_type}")
         logger.debug(f"Payload: {json.dumps(self.payload, indent=4)}")
--- src/webhook/hook.py
+++ src/webhook/hook.py
@@ -2,6 +2,7 @@ from aiohttp import web
 
 from utils.custom_logger import logger
 from src.sonarr.client import SonarrWebhookHandler
+from src.radarr.client import RadarrWebhookHandler
 
 class HandleWebHook:
     # Initialize the webhook receiver
@@ -11,7 +12,7 @@ class HandleWebHook:
         self.port = port
         self.app = web.Application()
         self.app.router.add_post('/sonarr_webhook', self.handle_sonarr)
-        #self.app.router.add_post('/radarr_webhook', self.handle_radarr)
+        self.app.router.add_post('/radarr_webhook', self.handle_radarr)
         #self.app.router.add_post('/plex_webhook', self.handle_plex)
         self.uvicorn_params = {
             "host": self.host,
@@ -27,6 +28,18 @@ class HandleWebHook:
             await sonarr_handler.handle_webhook()
         except Exception as e:
             logger.error(f"Error handling Sonarr webhook: {e}")
+            return web.Response(text='Error', status=500)
+
+        return web.Response(text='OK')
+    
+    # Handle Radarr webhook
+    async def handle_radarr(self, request):
+        try:
+            payload = await request.json()
+            radarr_handler = RadarrWebhookHandler(payload, self.discord_bot)
+            await radarr_handler.handle_webhook()
+        except Exception as e:
+            logger.error(f"Error handling Radarr webhook: {e}")
             return web.Response(text='Error', status=500)
 
         return web.Response(text='OK')
