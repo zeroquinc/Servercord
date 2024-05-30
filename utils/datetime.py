70ca8198868ab15ab33d0ebdaedb960bb5ecea2d
Zeroquinc <desiler@protonmail.com>
2024-05-31 01:55:20 +0200

    feature: delay tasks

--- utils/datetime.py
+++ utils/datetime.py
@@ -1,6 +1,22 @@
 from datetime import datetime, timedelta
 
-def get_times():
-    now = datetime.now()
-    one_hour_ago = now - timedelta(hours=1)
-    return now, one_hour_ago
+class TimeCalculator:
+    @staticmethod
+    def get_time_ago(hours=0, minutes=0):
+        now = datetime.now()
+        time_ago = now - timedelta(hours=hours, minutes=minutes)
+        return now, time_ago
+    
+    @staticmethod
+    def seconds_until_next_hour():
+        now = datetime.now()
+        next_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+        delta = next_hour - now
+        return delta.seconds
+
+    @staticmethod
+    def seconds_until_next_day():
+        now = datetime.now()
+        next_day = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+        delta = next_day - now
+        return delta.seconds
