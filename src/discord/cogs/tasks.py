1f5755cc673b0df7c8941bd0a8a349d5913a4917
zeroquinc <desiler@protonmail.com>
2024-06-27 11:58:23 +0200

    feature: add global trakt weekly watchers

--- src/discord/cogs/tasks.py
+++ src/discord/cogs/tasks.py
@@ -1,11 +1,13 @@
 import discord
 from discord.ext import tasks, commands
 import asyncio
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from utils.custom_logger import logger
 from utils.datetime import TimeCalculator
-from src.trakt.functions import process_ratings, process_favorites
+from src.trakt.ratings import process_ratings
+from src.trakt.favorites import process_favorites
+from src.trakt.global_weekly import create_weekly_global_embed
 from src.linux.disk_space import get_disk_space
 from config.globals import TRAKT_CHANNEL, TRAKT_USERNAME, ENABLE_DELAY, DISCORD_SERVER_ID
 
@@ -15,6 +17,7 @@ class TasksCog(commands.Cog):
         self.trakt_ratings.start()
         self.trakt_favorites.start()
         self.update_disk_space_channel.start()
+        self.weekly_global_embed.start()
 
     # Task to process recent Trakt ratings
     @tasks.loop(minutes=60)
@@ -64,6 +67,17 @@ class TasksCog(commands.Cog):
                 logger.info(f"Guild with ID {guild_id} not found.")
         except Exception as e:
             logger.error(f"Failed to update disk space channel: {e}")
+    
+    # Task to send a weekly global Trakt embed
+    @tasks.loop(hours=168)  # 168 hours in a week
+    async def weekly_global_embed(self):
+        channel = self.bot.get_channel(TRAKT_CHANNEL)
+        try:
+            embed_data = create_weekly_global_embed()
+            for embed in embed_data["embeds"]:
+                await channel.send(embed=discord.Embed.from_dict(embed))
+        except Exception as e:
+            logger.error(f'Error processing weekly Trakt embed: {e}')
 
     @trakt_ratings.before_loop
     async def before_trakt_ratings(self):
@@ -78,6 +92,26 @@ class TasksCog(commands.Cog):
             seconds = TimeCalculator.seconds_until_next_day()
             logger.info(f'Trakt favorites task will start in {str(timedelta(seconds=seconds))}')
             await asyncio.sleep(seconds)
+            
+    @weekly_global_embed.before_loop
+    async def before_weekly_task(self):
+        if ENABLE_DELAY:
+            await self.bot.wait_until_ready()  # Wait until the bot is ready
+            now = datetime.now()
+            # Calculate the next Wednesday
+            next_wednesday = now + timedelta((2-now.weekday()) % 7)  # 2 is Wednesday
+            # Calculate the time until the next Wednesday at 12 PM
+            next_run_time = datetime.combine(next_wednesday, datetime.min.time()) + timedelta(hours=12)
+            if now.hour >= 12:  # If it's past 12 PM, schedule for next week
+                next_run_time += timedelta(days=7)
+            # Calculate the sleep duration
+            sleep_duration = (next_run_time - now).total_seconds()
+            # Convert sleep_duration into hours, minutes, and seconds
+            hours, remainder = divmod(int(sleep_duration), 3600)
+            minutes, seconds = divmod(remainder, 60)
+            # Format the logging message to display time in HH:MM:SS format
+            logger.info(f'Weekly global Trakt task will start in {hours:02}:{minutes:02}:{seconds:02}')
+            await asyncio.sleep(sleep_duration)  # Sleep until the next run time
 
 async def setup(bot):
     logger.info('Cogs have been loaded')
--- src/trakt/favorites.py
+++ src/trakt/favorites.py
@@ -6,41 +6,6 @@ from config.globals import TRAKT_ICON
 
 from utils.custom_logger import logger
 
-# Function to process recent ratings and send them to a specified Discord channel as embeds
-async def process_ratings(ratings_channel, username):
-    client = TraktClient()
-    user = client.user(username)
-    now, then = TimeCalculator.get_time_ago(hours=1)
-    ratings = sorted(user.get_ratings(start_time=then, end_time=now), key=lambda rating: rating.date)
-
-    async def process_rating(rating):
-        author_formats = {
-            'show': "A show has been rated on Trakt",
-            'season': "A season has been rated on Trakt",
-            'episode': "An episode has been rated on Trakt",
-            'movie': "A movie has been rated on Trakt"
-        }
-        title_formats = {
-            'show': f"{rating.show_title}",
-            'season': f"{rating.show_title} - Season {rating.season_id}",
-            'episode': f"{rating.show_title} - S{rating.season_id}E{rating.episode_id}",
-            'movie': f"{rating.title} ({rating.year})"
-        }
-        author = author_formats[rating.type]
-        title = title_formats[rating.type]
-        embed_builder = EmbedBuilder(title=title, color=0xFF0000, url=rating.url)
-        embed_builder.add_field(name="User", value=f"[{username}](https://trakt.tv/users/{username})", inline=True)
-        embed_builder.add_field(name="Rating", value=f"{rating.rated} :star:", inline=True)
-        embed_builder.set_thumbnail(url=rating.poster)
-        embed_builder.set_author(name=author, icon_url=TRAKT_ICON)
-
-        logger.info(f"Sending rating embed to Discord: {title}")
-
-        await embed_builder.send_embed(ratings_channel)
-
-    for rating in ratings:
-        await process_rating(rating)
-
 # Function to process recent favorites and send them to a specified Discord channel as embeds
 async def process_favorites(favorites_channel, username):
     client = TraktClient()
